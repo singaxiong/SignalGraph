824fec4b23779bb6d962512b8529b4e130c7fd4a
Xiong Xiao <xiao.xiong.1981@gmail.com>
2016-07-07 16:02:03 +0800

    Revise trainGraph_Batch_LBFGS.m

--- tools/trainGraph_Batch_LBFGS.m
+++ tools/trainGraph_Batch_LBFGS.m
@@ -10,17 +10,17 @@
 %
 % Author: Xiong Xiao, Temasek labs, NTU, Singapore.
 % Date Created: 10 Oct 2015
-% Last Modified: 04 Jul 2016
+% Last Modified: 07 Jul 2016
 %
 %
-function layer = trainGraph_Batch_LBFGS(layer, data, data_t, para, LOG)
+function [layer, para, LOG] = trainGraph_Batch_LBFGS(layer, data, data_t, para, LOG)
 para = ParseOptions2(para);
 layer = initializeParametersDNN_tree(layer, para);    % Use random initialization if a weight matrix is not defined.
 layer = setDNNParameterPrecision(layer, para.singlePrecision, para.useGPU);
 LOG = initializeLog(LOG);
 para.NET.WeightUpdateOrder = genWeightUpdateOrder(layer, para.NET.WeightTyingSet);
 
-[filepath] = fileparts(para.output);    mkdir(filepath);
+% [filepath] = fileparts(para.output);    mkdir(filepath);
 
 % --------------- Test on crossvalidation data before training -------- %
 if ~isempty(data_t) && para.skipInitialEval==0
@@ -30,33 +30,40 @@ end
 
 % --------------- START THE TRAINING --------------- %
 
-startItr = length(LOG.actual_LR)+1;
-
 para.NET.sentenceMinibatch = para.NET.sequential;
 [minibatch] = MinibatchPackaging_tree4(data, para);
 batch_data = minibatch.data(:,1);
 
+% ------------- Check whether the gradient is correct ------------- %
+if para.checkGradient ==1
+    computeNumericalGradientLayer_tree2(layer, batch_data, para);
+end
+
+options.maxIter = para.NET.maxItrLBFGS;
+options.Method = 'LBFGS';
+options.display = 0;    % disable the message from the minFunc package
+
+mode = 1;
+startItr = length(LOG.actual_LR)+1;
+theta = NetWeights_layer2vec(layer, 0, para.useGPU);
 for itr = startItr:para.maxItr
+    old_theta = theta;
+    [theta, cost] = minFunc( @(x) DNN_cost_wrapper(x, layer, batch_data, para, mode), theta, options); % optimization
+    LOG.cost(itr) = gather(cost);
+
+    layer = NetWeights_vec2layer(theta, layer, 0);
     
-    % --------------- optional gradient check -------------------- %
-    % ------------- Check whether the gradient is correct ------------- %
-    if para.checkGradient ==1
-        computeNumericalGradientLayer_tree2(layer, batch_data, para);
-        para.checkGradient = 0; % do it only once
-    end
-    
-    options.maxIter = para.NET.maxItrLBFGS;
-    options.Method = 'LBFGS';
-    options.display = 1;
-    
-    mode = 1;
-    initW = NetWeights_layer2vec(layer, 0, para.useGPU);
-    [opttheta, cost(itr)] = minFunc( @(x) DNN_cost_wrapper(x, layer, batch_data, para, mode), initW, options); % optimization
-    layer = NetWeights_vec2layer(opttheta, layer, 0);
+    fprintf('Cost for epoch %d = %f - %s\n', itr, LOG.cost(itr), datestr(now));
     
     % ----------- Evaluate on crossvalidation data ------------- %    
     if ~isempty(data_t)
         fprintf('Evaluating on cross-validation data - %s\n', datestr(now));
         [LOG.cost_cv(itr), cost_pure_cv, LOG.subcost_cv(:,itr), LOG.subacc_cv(:,itr)] = CrossValidationTest_tree4(layer, data_t, para);
+    end
+    
+    % stopping criterion
+    if itr>=para.minItr && (LOG.cost(end-1)-LOG.cost(end))/LOG.cost(end) <para.NET.stopImprovement/100
+        fprintf('Improvement is less than %2.3f%%, stop the training!\n', para.NET.stopImprovement);
+        break;
     end
 end
