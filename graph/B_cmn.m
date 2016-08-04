6878fb7d80d128dd0f335b29e2d84888bd61b0da
Xiong Xiao <xiao.xiong.1981@gmail.com>
2016-08-04 16:01:17 +0800

    debug B_cmn.m

--- graph/B_cmn.m
+++ graph/B_cmn.m
@@ -6,13 +6,18 @@ if N==1
     grad = CMN(future_grad')';
 else
     [mask, variableLength] = getValidFrameMask(future_layers{1});
-    future_grad2 = ExtractVariableLengthTrajectory(future_grad, mask);
-    for i=1:N
-        grad{i} = CMN(future_grad2{i}')';
-    end
-    grad = cell2mat_gpu(grad);
     if variableLength
+        future_grad2 = ExtractVariableLengthTrajectory(future_grad, mask);
+        for i=1:N
+            grad{i} = CMN(future_grad2{i}')';
+        end
+        grad = cell2mat_gpu(grad);
+        
         grad = PadGradientVariableLength(grad, mask);
+    else
+        future_grad2 = reshape(permute(future_grad, [1 3 2]), D*N,T);
+        grad = CMN(future_grad2')';
+        grad = permute(reshape(grad, D, N, T), [1 3 2]);
     end
 end
 
