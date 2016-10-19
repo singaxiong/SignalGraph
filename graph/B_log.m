34e7bc6ca04be946f45bc89d23cb599d283397e7
Xiong Xiao <xiao.xiong.1981@gmail.com>
2016-10-19 09:43:17 +0800

    add B_add
    Former-commit-id: c71b79a4f15f24b0de52be0d26668684f91aaffb

--- graph/B_log.m
+++ graph/B_log.m
@@ -1,8 +1,8 @@
 
-function grad = B_log(future_layers, input,b)
-future_grad = GetFutureGrad(future_layers, {});
+function grad = B_log(future_layers, input,curr_layer)
+future_grad = GetFutureGrad(future_layers, curr_layer);
 
-grad = 1./(input+b).*future_grad;
+grad = 1./(input+curr_layer.const).*future_grad;
 
 
 end
